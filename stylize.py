f30350e5de62a60e8bf1353327f06843e5122bdd
ivaaane <ivanzaragoza2007@gmail.com>
2024-06-05 14:30:12 +0200

    Initial commit

--- stylize.py
+++ stylize.py
@@ -2,12 +2,17 @@
 # generate_board() and make it more visually pleasing
 # in the console.
 
-from board_generator import generate_board
+from minesweeper_generator import generate_board
 
-board = generate_board(size=16, bombs=40)
+board = generate_board(size_x=30, size_y=16, bombs=99)
+
 for i in board:
     line = ""
     for j in i:
-        line += str(j)
-        line += " "
+        if j == 0:
+            line += "· "
+        elif j == -1:
+            line += "⬤ "
+        else:
+            line += str(j) + " "
     print(line)
